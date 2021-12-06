cb0b0c085b288d003524cba5dc4660b1acd68e76
Rahul Kathait <edendattox@gmail.com>
2021-12-06 23:40:53 +0530

    Created secondary button

---
+++ src/components/ButtonSecondary/ButtonSecondary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ButtonSecondary = ({ name, type, onClick }) => {
+  return <button className="buttonSecondary">{name}</button>;
+};
+
+export default ButtonSecondary;
