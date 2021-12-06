377fac3bd55a19b87bf6585279adc709d25709bf
Rahul Kathait <edendattox@gmail.com>
2021-12-06 23:41:26 +0530

    Added login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import LanguageOutlinedIcon from "@material-ui/icons/LanguageOutlined";
+import "./Login.css";
+import ButtonPrimary from "../../components/ButtonPrimary/ButtonPrimary";
+import ButtonSecondary from "../../components/ButtonSecondary/ButtonSecondary";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <div className="login__header">
+        <div className="login__logo">
+          <Link to="/">
+            <img
+              src="https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png"
+              alt="#"
+            />
+          </Link>
+        </div>
+        <div className="login__language">
+          <LanguageOutlinedIcon />
+          <span>en-US</span>
+        </div>
+      </div>
+      <div className="login__info">
+        <h1>Sign in</h1>
+        <form className="login__form">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <ButtonPrimary name="Sign In " type="submit" onClick={signIn} />
+        </form>
+        <div className="login__divider">
+          <hr />
+          <span>OR</span>
+          <hr />
+        </div>
+        <Link to="/sign up">
+          <ButtonSecondary name="create account" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
